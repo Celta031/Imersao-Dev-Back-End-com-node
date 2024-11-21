019cd3504a3a5352f5fad441fd610bd17b32d805
Celta031 <wesleyraphael2003@gmail.com>
2024-11-21 20:22:12 -0300

    aula2

---
+++ server.js
@@ -0,0 +1,56 @@
+import express from "express";
+
+const posts = [
+    {
+        id:1,
+        descricao: "Uma foto teste",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id:2,
+        descricao: "Paisagem deslumbrante",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id:3,
+        descricao: "Comida deliciosa",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id:4,
+        descricao: "Gato fofo dormindo",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id:5,
+        descricao: "Cachorro brincando no parque",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id:6,
+        descricao: "Montanha coberta de neve",
+        imagem: "https://placecats.com/millie/300/150"
+    }
+];
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log("Servidor escutando...");
+});
+
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
+});
